af5c361fc62f8030502d763556b9a3c6c5e1570e
MD PARVESH <abraartishan@Mds-MacBook-Pro.local>
2021-01-29 20:38:19 -0500

    test

--- INTELLIJ/src/test/java/CucumberRunner/RunCukesTest.java
+++ INTELLIJ/src/test/java/CucumberRunner/RunCukesTest.java
@@ -52,8 +52,8 @@ public class RunCukesTest extends AbstractTestNGCucumberTests {
             options.addArguments("--disable-gpu");
             options.addArguments("--disable-infobars");
             options.addArguments("--disable-dev-shm-usage");
-//            options.addArguments("--remote-debugging-port=9515");
-            options.addArguments("--remote-debugging-port=9222");
+            options.addArguments("--remote-debugging-port=9515");
+//            options.addArguments("--remote-debugging-port=9222");
 
 //            options.setExperimentalOption("useAutomationExtension", false);
 //            , start-maximized
